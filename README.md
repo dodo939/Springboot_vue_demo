5dc461fae7be1f3071b06790e7548e419cf5e0b5
dodo939 <yangfuyu83@gmail.com>
2024-08-31 01:15:51 +0800

    将敏感信息通过环境变量传递给程序；添加 README.md 文件

---
+++ README.md
@@ -0,0 +1,2 @@
+## Tips
+- 运行前需按照 <u>**[application.yml](src/main/resources/application.yml)**</u> 将环境变量添加到系统变量中
--- src/main/java/io/github/dodo939/utils/AliOssUtil.java
+++ src/main/java/io/github/dodo939/utils/AliOssUtil.java
@@ -4,21 +4,40 @@ import com.aliyun.oss.ClientException;
 import com.aliyun.oss.OSS;
 import com.aliyun.oss.OSSClientBuilder;
 import com.aliyun.oss.OSSException;
-import com.aliyun.oss.common.auth.CredentialsProviderFactory;
-import com.aliyun.oss.common.auth.EnvironmentVariableCredentialsProvider;
 import com.aliyun.oss.model.PutObjectRequest;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 import java.io.InputStream;
 
+@Component
 public class AliOssUtil {
-    private static final String ENDPOINT = "https://oss-cn-beijing.aliyuncs.com";
-    private static final String BUCKET_NAME = "dodo-oss-bucket";
+    @Value("${AliOSS.endpoint}")
+    private String endpoint;
+    @Value("${AliOSS.bucket-name}")
+    private String bucketName;
+    @Value("${AliOSS.access-key-id}")
+    private String accessKeyId;
+    @Value("${AliOSS.access-key-secret}")
+    private String accessKeySecret;
+
+    private static String ENDPOINT;
+    private static String BUCKET_NAME;
+    private static String ACCESS_KEY_ID;
+    private static String ACCESS_KEY_SECRET;
+
+    @PostConstruct
+    public void init() {
+        ENDPOINT = this.endpoint;
+        BUCKET_NAME = this.bucketName;
+        ACCESS_KEY_ID = this.accessKeyId;
+        ACCESS_KEY_SECRET = this.accessKeySecret;
+    }
 
     public static String uploadFile(String objectName, InputStream inputStream) throws Exception {
-        // 从环境变量中获取访问凭证。设置环境变量 OSS_ACCESS_KEY_ID 和 OSS_ACCESS_KEY_SECRET。
-        EnvironmentVariableCredentialsProvider credentialsProvider = CredentialsProviderFactory.newEnvironmentVariableCredentialsProvider();
 
-        OSS ossClient = new OSSClientBuilder().build(ENDPOINT, credentialsProvider);
+        OSS ossClient = new OSSClientBuilder().build(ENDPOINT, ACCESS_KEY_ID, ACCESS_KEY_SECRET);
 
         String url = "";
 
